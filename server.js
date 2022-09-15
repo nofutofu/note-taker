4c83f1ba23f5f82771e392a2e02577d6c7e6b1b4
nofutofu <aaronrweis@gmail.com>
2022-09-15 10:24:21 -0500

    finally fixed bug stopping me from fetching

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+// all imports
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-
+// writes the updated notes to the json file
 const writeToJson = (destination, content) =>
 fs.writeFile(destination, JSON.stringify(content, null, 3), (err) =>{
 if(err) { console.error(err)}}
 );
 
+// takes the current data from the json file and stores it in a variable
 const rewriteJson = (content, file) => {
 fs.readFile(file, 'utf8', (err, data) => {
     const parsedData = JSON.parse(data);
@@ -25,12 +26,15 @@ fs.readFile(file, 'utf8', (err, data) => {
 });
 };
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+// get request for the default note page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
+// posts the incoming note to the json file
 app.post('/api/notes', (req, res) => {
     console.log(req.body);
+    if(req.body) {
     const {title, text} = req.body;
 
         const newNote = {
@@ -38,21 +42,32 @@ app.post('/api/notes', (req, res) => {
           text,
           id: uuidv4(),
         };
-
-          rewriteJson(newNote, './db/db.json');
+          rewriteJson(newNote, './db/notes.json');
+          res.json('Note has been added!');
+        } else {
+            res.error('Error in adding note. :*(');
+        }
 });
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+// gets the raw data from the json file
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/notes.json')));
 
+// deletes the specified note by id from the json file
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
         let parsedData = JSON.parse(data);
         const deleted = parsedData.find(data => data.id === req.params.id)
         if (deleted) {
             parsedData = parsedData.filter(data => data.id != req.params.id)
+            res.json('Deleted note');
+            writeToJson('./db/notes.json', parsedData);
+        } else {
+            res.json('Failed to delete note.');
         }
-        writeToJson('./db/db.json', parsedData);
     });
 });
+
+// defaults to the landing page if any other path it selected
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}`));
